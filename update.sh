d6d4b9bc827260ee6b0349aa8a607504b1c09c56
YogSottot <7411302+YogSottot@users.noreply.github.com>
2024-10-23 22:49:23 +0600

    move repo to subdir

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ set -eo pipefail
 DISTRIBUTIONS=("bookworm" "noble")
 MODULES=("mod_ntlm" "mod_zip" "mod_zstd")
 
+cd debian
+
 for dist in "${DISTRIBUTIONS[@]}"; do
     echo "Processing distribution: $dist"
     
     #for module in "${MODULES[@]}"; do
         #echo "Including $module for $dist"
             #find "../nginx_mod_zip_docker_builder/fpm/$module/$dist" -name "*${dist}*.deb" -exec reprepro includedeb "$dist" {} +
-            find "../nginx_mod_zip_docker_builder/fpm/deb/$dist" -name "*${dist}*.deb" -exec reprepro includedeb "$dist" {} +
+            find "../../nginx_mod_zip_docker_builder/fpm/deb/$dist" -name "*${dist}*.deb" -exec reprepro includedeb "$dist" {} +
     #done
 done
