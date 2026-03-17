8df7ffaf52529559e457da21648838d2c64b2702
YogSottot <7411302+YogSottot@users.noreply.github.com>
2026-03-17 13:13:38 +0300

    add rpm zellij

---
+++ update_rpm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+WORKDIR=./redhat/repo/x86_64
+
+mkdir -p ${WORKDIR}
+cp ../nginx_mod_zip_docker_builder/zellij/rpm_zellij/rpmbuild/RPMS/x86_64/*.rpm ${WORKDIR}
+
+# подпись
+source ./redhat/gpg-env.sh
+rpm \
+  --define "_signature gpg" \
+  --define "_gpg_name ${RPM_GPG_NAME}" \
+  --define "_gpg_path ${RPM_GPG_PATH}" \
+  --define "__gpg ${RPM_GPG_BIN}" \
+  --addsign  ${WORKDIR}/*.rpm
+
+# обновляем репо
+createrepo_c --update ${WORKDIR}
+
+
+gpg --local-user E52900A31721FCFE \
+    --detach-sign --armor \
+    ${WORKDIR}/repodata/repomd.xml
